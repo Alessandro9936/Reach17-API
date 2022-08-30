4bc17a1e9408d53d2eb7a330847b45d26e2e45de
Alessandro Crinella <alecrinella99@gmail.com>
2022-08-30 12:27:15 +0200

    Handle creation of a new course

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,1 +1,61 @@
-// Middleware function to handle course routes
+const { body, validationResult } = require("express-validator");
+
+const Course = require("../models/courseModel");
+const Goal = require("../models/goalModel");
+
+exports.courses_list = (req, res, next) => {};
+
+exports.course_create_post = [
+  // First / second middleware function --> Validate form input values
+  body("name")
+    .notEmpty()
+    .withMessage("Name field must not be empty")
+    .custom((value) => {
+      return Course.findOne({ name: value }).then((course) => {
+        if (course) throw new Error(`${value} already exists`);
+      });
+    }),
+  body("description")
+    .notEmpty()
+    .withMessage("Description field must not be empty"),
+
+  // Third middleware function --> Handle errors from validation or save new course
+  (req, res, next) => {
+    // Check if validation returned errors
+    const errors = validationResult(req);
+
+    // If validation result got errors return them
+    if (!errors.isEmpty()) {
+      res.json({ errors: errors.array() });
+      return;
+    }
+
+    // Retrieve reference to course's goals to store them in new course schema with other values
+    Goal.find({ name: req.body.goals })
+      .select("_id")
+      .exec((err, goals) => {
+        if (err) {
+          return next(err);
+        }
+
+        const course = new Course({
+          name: req.body.name,
+          description: req.body.description,
+          goals,
+          athenaeum: req.body.athenaeums,
+        });
+
+        // Validation passed, store new course in database and send it back
+        course.save((err) => {
+          if (err) return next(err);
+          res.json({ course });
+        });
+      });
+  },
+];
+
+exports.course_update_post = (req, res, next) => {};
+
+exports.course_delete_post = (req, res, next) => {};
+
+exports.course_detail = (req, res, next) => {};
--- routes/course.js
+++ routes/course.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const courseController = require("../controllers/courseController");
+
+router.get("/", courseController.courses_list);
+
+router.post("/", courseController.course_create_post);
+router.put("/:id", courseController.course_update_post);
+router.delete("/:id", courseController.course_delete_post);
+
+router.get("/:id", courseController.course_detail);
+
 module.exports = router;
