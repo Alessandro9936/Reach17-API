1916e1ea989b1657eed4e5b6522d7a1a2d5cba17
Alessandro Crinella <alecrinella99@gmail.com>
2022-08-31 08:05:30 +0200

    Update code to use async await instead of promises for cleaner structure

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -10,47 +10,42 @@ exports.course_create_post = [
   body("name")
     .notEmpty()
     .withMessage("Name field must not be empty")
-    .custom((value) => {
-      return Course.findOne({ name: value }).then((course) => {
-        if (course) throw new Error(`${value} already exists`);
-      });
+    .custom(async (value) => {
+      const existingCourse = await Course.findOne({ name: value });
+      if (existingCourse) {
+        throw new Error(`${value} already exists`);
+      }
     }),
   body("description")
     .notEmpty()
     .withMessage("Description field must not be empty"),
 
   // Third middleware function --> Handle errors from validation or save new course
-  (req, res, next) => {
+  async (req, res, next) => {
     // Check if validation returned errors
     const errors = validationResult(req);
 
     // If validation result got errors return them
     if (!errors.isEmpty()) {
-      res.json({ errors: errors.array() });
+      res.status(400).json({ errors: errors.array() });
       return;
     }
 
     // Retrieve reference to course's goals to store them in new course schema with other values
-    Goal.find({ name: req.body.goals })
-      .select("_id")
-      .exec((err, goals) => {
-        if (err) {
-          return next(err);
-        }
+    const courseGoals = await Goal.find({ name: req.body.goals }).select("_id");
 
-        const course = new Course({
-          name: req.body.name,
-          description: req.body.description,
-          goals,
-          athenaeum: req.body.athenaeums,
-        });
+    const course = new Course({
+      name: req.body.name,
+      description: req.body.description,
+      goals: courseGoals,
+      athenaeum: req.body.athenaeums,
+    });
 
-        // Validation passed, store new course in database and send it back
-        course.save((err) => {
-          if (err) return next(err);
-          res.json({ course });
-        });
-      });
+    // Validation passed, store new course in database and send it back
+    course.save((err) => {
+      if (err) return next(err);
+      res.json({ course });
+    });
   },
 ];
 
