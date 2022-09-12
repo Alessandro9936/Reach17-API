a83ecc08ac51473662883f0a7cfae855d4ce42bd
Alessandro Crinella <alecrinella99@gmail.com>
2022-09-12 12:22:22 +0200

    Implement test for create and read an athenaeum

--- routes/__test__/ahenaeum.test.js
+++ routes/__test__/ahenaeum.test.js
@@ -1,12 +1,51 @@
 const app = require("../../app");
 const request = require("supertest");
+const db = require("../../configs/db.config.testing");
 
-const { dbConnect, dbDisconnect } = require("../../mongoConfigTesting");
+const athenaeum = require("../../models/athenaeumModel");
 
-beforeAll(() => {
-  dbConnect();
+const agent = request.agent(app);
+
+beforeAll(async () => {
+  await db.connect();
 });
 
-afterAll(async () => await dbDisconnect());
+beforeEach(async () => {
+  await db.clear();
+});
 
-//Crate before ay action
+afterAll(async () => {
+  await db.close();
+});
+
+describe("POST /athenaems => Create a new athenaeum", () => {
+  test("Create successfully a new athenaeum", (done) => {
+    agent
+      .post("/athenaeums")
+      .send({ name: "athenaeum" })
+      .expect(201)
+      .then((res) => {
+        ////
+
+        athenaeum.countDocuments().then((doc) => console.log(doc));
+        ///
+
+        athenaeum.findById(res.body._id).then((ath) => {
+          expect(ath.name).toEqual("athenaeum");
+          expect(ath._id).toBeTruthy();
+        });
+        done();
+      });
+  });
+
+  test("fail to create a new athenaeum if input fields are empty", (done) => {
+    agent
+      .post("/athenaeums")
+      .send({ name: "" })
+      .expect(400)
+      .then((res) => {
+        expect(res.error).toBeTruthy();
+        done();
+      });
+  });
+});
