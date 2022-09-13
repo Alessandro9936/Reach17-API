8ed1daa40c9bb246944f131443a02b14e64bb79a
Alessandro Crinella <alecrinella99@gmail.com>
2022-09-13 11:37:54 +0200

    Add functionality that seed in memory database used when a test need data

--- configs/db.config.testing.js
+++ configs/db.config.testing.js
@@ -1,31 +1,53 @@
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 
+const Athenaeum = require("../models/athenaeumModel");
+const Course = require("../models/courseModel");
+const Goal = require("../models/goalModel");
+
+const { athenaeumsData, coursesData, goalsData } = require("./seedData");
+
 let mongoServer;
 
 // Provide connection to a new in-memory databse server
 const connect = async () => {
-  // before establishing a new connection close previously
-  await mongoose.disconnect();
+  try {
+    // before establishing a new connection close previously
+    await mongoose.disconnect();
 
-  mongoServer = await MongoMemoryServer.create();
-  const mongoUri = await mongoServer.getUri();
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUri = await mongoServer.getUri();
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.error("Failed to connect to in-memory database:" + err);
+  }
 };
 
 const close = async () => {
-  await mongoose.disconnect();
-  await mongoServer.stop();
+  try {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const clear = async () => {
-  const collections = mongoose.connection.collections;
+  try {
+    const collections = mongoose.connection.collections;
 
-  for (const key in collections) {
-    await collections[key].deleteMany();
+    for (const key in collections) {
+      await collections[key].deleteMany();
+    }
+
+    await Athenaeum.insertMany(athenaeumsData);
+    await Course.insertMany(coursesData);
+    await Goal.insertMany(goalsData);
+  } catch (err) {
+    console.error(err);
   }
 };
 
---
+++ configs/seedData.js
@@ -0,0 +1,23 @@
+const athenaeumsData = [
+  { name: "athenaeum1", courses: [] },
+  { name: "athenaeum2", courses: [] },
+  { name: "athenaeum3", courses: [] },
+];
+
+const goalsData = [
+  { name: "goal1", description: "goal1", courses: [] },
+  { name: "goal2", description: "goal2", courses: [] },
+  { name: "goal3", description: "goal3", courses: [] },
+];
+
+const coursesData = [
+  { name: "course1", description: "course1", goals: [], athenaeums: [] },
+  { name: "course2", description: "course2", goals: [], athenaeums: [] },
+  { name: "course3", description: "course3", goals: [], athenaeums: [] },
+];
+
+module.exports = {
+  athenaeumsData,
+  goalsData,
+  coursesData,
+};
