be1694e88d38005453bf11a4fe7884a4d843c2c1
Alessandro Crinella <alecrinella99@gmail.com>
2022-09-19 09:05:17 +0200

    Since not required remove description field from schemas

--- models/courseModel.js
+++ models/courseModel.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const courseSchema = new Schema(
   {
     name: { type: String, required: true },
-    description: { type: String, required: true },
     goals: [{ type: Schema.Types.ObjectId, ref: "Goal" }],
     athenaeums: [{ type: Schema.Types.ObjectId, ref: "Athenaeum" }],
   },
